46402074944d39e3dfe02c7a5c167fbb49f3574b
Steven Doran <steven.doran@flatironschool.com>
2019-12-02 13:46:05 -0500

    pre lecture

---
+++ 21-sessions-and-cookies/amazon/db/migrate/20191202184122_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :description
+      t.integer :year
+      t.integer :page_count
+
+      t.timestamps
+    end
+  end
+end
